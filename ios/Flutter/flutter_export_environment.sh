e5ed1ab70b120dc4d4f6d63c3cba2b5e9d70014b
Bono Lv <lvscar@gmail.com>
2020-02-11 12:47:41 +0800

    running on iOS simulator the first time

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/bigzhu/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bigzhu/english_buoy"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/lvs/flutter_dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lvs/Code/github-repos/english_buoy"
+export "FLUTTER_TARGET=/Users/lvs/Code/github-repos/english_buoy/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/bigzhu/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/lvs/flutter_dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.4.01"
 export "FLUTTER_BUILD_NUMBER=1401"
+export "TRACK_WIDGET_CREATION=true"
