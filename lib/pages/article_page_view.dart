92d95749dc4d37666b46e2405e417979616f51e9
Bono Lv <lvscar@gmail.com>
2020-02-13 12:49:27 +0800

    add a floating button for backing to home for iOS

--- lib/pages/article_page_view.dart
+++ lib/pages/article_page_view.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 import '../models/article_titles.dart';
 import './article.dart';
 import '../models/controller.dart';
+import 'dart:io' show Platform;
+
 
 class ArticlePageViewPage extends StatefulWidget {
   @override
@@ -22,19 +24,8 @@ class _ArticlePageViewPage extends State<ArticlePageViewPage>
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    if (_controller.articlePageController == null) return Container();
-    return WillPopScope(
-      onWillPop: () async {
-        if (_controller.mainSelectedIndex == 1) {
-          _controller.setMainSelectedIndex(0);
-          return false;
-        } else
-          return true;
-      },
-      child: PageView(
+  Widget _pageView (BuildContext context ){
+      return PageView(
           reverse: true,
           onPageChanged: (i) {
             _articleTitles.currentArticleIndex = i;
@@ -44,7 +35,39 @@ class _ArticlePageViewPage extends State<ArticlePageViewPage>
           controller: _controller.articlePageController,
           children: _articleTitles.filterTitles.map((d) {
             return ArticlePage(d.id);
-          }).toList()),
+          }).toList());
+  }
+
+
+  Widget _buildForAndroid (BuildContext context){
+    return WillPopScope(
+      onWillPop: () async {
+        if (_controller.mainSelectedIndex == 1) {
+          _controller.setMainSelectedIndex(0);
+          return false;
+        } else
+          return true;
+      },
+      child: _pageView(context) ,
     );
+  }
+
+  Widget _buildForiOS ( BuildContext context){
+    return Scaffold(
+      appBar: null ,
+      body: _pageView(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { _controller.setMainSelectedIndex(0); },
+        tooltip: 'back to home',
+        child: Icon(Icons.home),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    if (_controller.articlePageController == null) return Container();
+    return Platform.isIOS ? _buildForiOS(context) : _buildForAndroid(context);
   }
 }
