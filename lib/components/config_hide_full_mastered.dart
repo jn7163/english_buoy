5285f11e0c7a2661bde41345d47580333726a786
bigzhu <vermiliondun@gmail.com>
2020-02-15 17:48:00 +0800

    fix test error

--- lib/components/config_hide_full_mastered.dart
+++ lib/components/config_hide_full_mastered.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
-import '../models/settings.dart';
 import '../models/article_titles.dart';
 
 class ConfigHideFullMastered extends StatelessWidget {
@@ -11,14 +10,12 @@ class ConfigHideFullMastered extends StatelessWidget {
   Widget build(BuildContext context) {
     ArticleTitles articleTitles =
         Provider.of<ArticleTitles>(context, listen: false);
-    return Consumer<Settings>(builder: (context, setting, child) {
-      return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text('Hide Full Mastered'),
-        Switch(
-          value: setting.isHideFullMastered,
-          onChanged: articleTitles.filterHideMastered,
-        )
-      ]);
-    });
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text('Hide Mastered'),
+      Switch(
+        value: articleTitles.settings.isHideFullMastered,
+        onChanged: articleTitles.filterHideMastered,
+      )
+    ]);
   }
 }
