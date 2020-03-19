40e28424ece42eb063549695476e2b2c55904ee0
bigzhu <vermiliondun@gmail.com>
2020-03-19 20:57:11 +0800

    clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-//import 'package:page_transition/page_transition.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:easy_alert/easy_alert.dart';
 import 'package:provider/provider.dart';
+
 import './models/oauth_info.dart';
 import './models/article.dart';
 import './models/loading.dart';
@@ -12,18 +14,11 @@ import './models/controller.dart';
 import './models/global.dart';
 import './models/explorer.dart';
 
-import './pages/waiting.dart';
-import './pages/article_titles.dart';
-import './pages/article.dart';
-import './pages/sign.dart';
-import './pages/add_article.dart';
-import './pages/guid.dart';
-import './pages/article_page_view.dart';
 import './pages/home.dart';
 
 import './themes/dark.dart';
 import './themes/bright.dart';
-import 'dart:async';
+
 import './store/wordwise.dart';
 import './store/store.dart';
 
@@ -51,7 +46,6 @@ class _EbuoyState extends State<Ebuoy> {
     Store.prefs;
     openDB();
     _articleTitles = ArticleTitles();
-
     _settings = Settings();
     _controller = Controller();
     // 绑定 setting 迸去
@@ -64,7 +58,6 @@ class _EbuoyState extends State<Ebuoy> {
   receiveShare(String sharedText) {
     if (sharedText == null) return;
     _controller.setMainSelectedIndex(0);
-    // 收到分享, 设置
     _articleTitles.newYouTube(sharedText);
   }
 
@@ -80,7 +73,6 @@ class _EbuoyState extends State<Ebuoy> {
 
     // For sharing or opening urls/text coming from outside the app while the app is closed
     ReceiveSharingIntent.getInitialText().then((String value) {
-      print("shared to closed app value=$value");
       receiveShare(value);
     });
   }
@@ -97,14 +89,11 @@ class _EbuoyState extends State<Ebuoy> {
         providers: [
           ChangeNotifierProvider(create: (_) => Explorer()),
           Provider<Global>(create: (_) => Global()),
-          //ChangeNotifierProvider(create: (_) => _controller),
           ChangeNotifierProvider.value(value: _controller),
           ChangeNotifierProvider(create: (_) => Article()),
           ChangeNotifierProvider(create: (_) => Loading()),
           ChangeNotifierProvider(create: (_) => OauthInfo()),
-          //ChangeNotifierProvider(create: (_) => _articleTitles),
           ChangeNotifierProvider.value(value: _articleTitles),
-          //ChangeNotifierProvider(create: (_) => _settings),
           ChangeNotifierProvider.value(value: _settings),
         ],
         child: Selector<Settings, bool>(
@@ -116,60 +105,7 @@ class _EbuoyState extends State<Ebuoy> {
                 darkTheme: darkTheme,
                 themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
                 home: HomePage(),
-                //onGenerateRoute: getRoute,
               );
             }));
-  }
-
-  Route getRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/ArticlePageView':
-        return _buildRoute(settings, ArticlePageViewPage());
-      /*
-        return PageTransition(
-          duration: Duration(milliseconds: 500),
-          type: PageTransitionType.rightToLeft,
-          child: ArticlePageViewPage(),
-          settings: settings,
-        );
-        */
-      case '/Guid':
-        return _buildRoute(settings, GuidPage());
-      /*
-        return PageTransition(
-          duration: Duration(milliseconds: 500),
-          type: PageTransitionType.rightToLeft,
-          child: GuidPage(),
-          settings: settings,
-        );
-        */
-      case '/Waiting':
-        return _buildRoute(settings, WaitingPage());
-      case '/ArticleTitles':
-        return _buildRoute(settings, ArticleTitlesPage());
-      case '/AddArticle':
-        return _buildRoute(settings, AddArticlePage());
-      case '/Article':
-        return _buildRoute(settings, ArticlePage(settings.arguments));
-      /*
-        return PageTransition(
-          duration: Duration(milliseconds: 500),
-          type: PageTransitionType.rightToLeft,
-          child: ArticlePage(settings.arguments),
-          settings: settings,
-        );
-        */
-      case '/Sign':
-        return _buildRoute(settings, SignInPage());
-      default:
-        return null;
-    }
-  }
-
-  MaterialPageRoute _buildRoute(RouteSettings settings, Widget builder) {
-    return MaterialPageRoute(
-      settings: settings,
-      builder: (BuildContext context) => builder,
-    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,55 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './article_titles.dart';
-import 'explorer.dart';
-import 'article_page_view.dart';
+import './explorer.dart';
+import './article_page_view.dart';
 import '../models/controller.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  Controller _controller;
-  //ArticleTitles _articleTitles;
-
-  static List<BottomNavigationBarItem> bottomNavigationBarItem =
-      <BottomNavigationBarItem>[
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      title: Text('Home'),
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.library_books),
-      title: Text('Article'),
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.local_library),
-      title: Text('Lib'),
-    ),
-  ];
-  @override
-  void initState() {
-    super.initState();
-    _controller = Provider.of<Controller>(context, listen: false);
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
+    return PageView(
         //disable scroll
         physics: NeverScrollableScrollPhysics(),
         children: [
           ArticleTitlesPage(),
           ArticlePageViewPage(),
           ExplorerPage(),
-          //Center(child: Text('Developing')),
         ],
-        controller: _controller.mainPageController,
-        onPageChanged: (index) {},
-      ),
-    );
+        controller:
+            Provider.of<Controller>(context, listen: false).mainPageController);
   }
 }
